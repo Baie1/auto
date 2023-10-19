d55b4d7849e3c447d47f8959457001b9ddd444fc
baie1 <valenteylor@gmail.com>
2023-10-20 00:52:12 +0600

    auto

---
+++ redm/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import React from "react";
+import Info from "./components/Info";
+import './styles/global.css'
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Info/>
+    </div>
+  );
+}
+
+export default App;
---
+++ redm/src/components/Info.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import styles from './Info.module.css'
+
+const Info = () => {
+    return (
+        <div className={styles.content}>
+            <div className={styles.client}>
+                <div className={styles.inform}>
+                    <h2 className={styles.forum}>
+                        Наши клиенты
+                    </h2>
+                    <h3 className={styles.forum}>Их истории</h3>
+                    <h3 className={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto assumenda aut blanditiis consequatur corporis
+                        dolore dolorem doloremque, dolores doloribus eaque earum
+                        eligendi est exercitationem harum illo incidunt magni numquam
+                        perspiciatis placeat quo, repellendus, rerum tempore voluptates! E
+                        aque ipsa officia optio recusandae. Adipisci aliquid corporis
+                        distinctio dolore ea facilis ipsa laboriosam nostrum nulla officia
+                        placeat totam, voluptas. Aut debitis ipsum iure magnam ratione
+                        recusandae sapiente? Commodi ipsum itaque, nostrum odio quod ratione
+                        saepe vitae. Eveniet nostrum quasi unde veniam voluptatem!
+                        Accusamus animi asperiores aspernatur consequatur, culpa cupiditate
+                        et expedita inventore ipsum iure mollitia nisi numquam odit placeat
+                        quidem repudiandae rerum sapiente sint sit temporibus, veniam vitae
+                        voluptate? Culpa dicta dolorem esse, eveniet exercitationem fugiat
+                        itaque iure magni natus nemo nisi placeat, quaerat qui sit, suscipit
+                        unde vitae! Deleniti eligendi et, fuga magnam necessitatibus praesentium
+                        quas vero? Accusamus amet cumque facer
+                    </h3>
+
+                    <div className={styles.footer}>
+                        <p>contacts</p>
+                        <a className={styles.href2} href="/">WhatsApp</a>
+                        <a className={styles.href2} href="/">Instagram</a>
+                        <a className={styles.href2} href="/">Telegram</a>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.brand}>
+
+                <div className={styles.inform}>
+                    <h2 className={styles.forum}>
+                         Известные бренды
+                    </h2>
+                    <h3 className={styles.forum}>Наименование</h3>
+                    <h3 className={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto assumenda aut blanditiis consequatur corporis
+                        dolore dolorem doloremque, dolores doloribus eaque earum
+                        eligendi est exercitationem harum illo incidunt magni numquam
+                        perspiciatis placeat quo, repellendus, rerum tempore voluptates! E
+                        aque ipsa officia optio recusandae. Adipisci aliquid corporis
+                        distinctio dolore ea facilis ipsa laboriosam nostrum nulla officia
+                        placeat totam, voluptas. Aut debitis ipsum iure magnam ratione
+                        recusandae sapiente? Commodi ipsum itaque, nostrum odio quod ratione
+                        saepe vitae. Eveniet nostrum quasi unde veniam voluptatem!
+                        Accusamus animi asperiores aspernatur consequatur, culpa cupiditate
+                        et expedita inventore ipsum iure mollitia nisi numquam odit placeat
+                        quidem repudiandae rerum sapiente sint sit temporibus, veniam vitae
+                        voluptate? Culpa dicta dolorem esse, eveniet exercitationem fugiat
+                        itaque iure magni natus nemo nisi placeat, quaerat qui sit, suscipit
+                        unde vitae! Deleniti eligendi et, fuga magnam necessitatibus praesentium
+                        quas vero? Accusamus amet cumque facer
+                    </h3>
+
+                   <div className={styles.footer}>
+                        <p>contacts</p>
+                        <a className={styles.href2} href="/">WhatsApp</a>
+                        <a className={styles.href2} href="/">Instagram</a>
+                        <a className={styles.href2} href="/">Telegram</a>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.desc}>
+
+                <div className={styles.inform}>
+                    <h2 className={styles.forum}>Документация</h2>
+                    <h3 className={styles.forum}>Ознакомтесь</h3>
+                    <h3 className={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto assumenda aut blanditiis consequatur corporis
+                        dolore dolorem doloremque, dolores doloribus eaque earum
+                        eligendi est exercitationem harum illo incidunt magni numquam
+                        perspiciatis placeat quo, repellendus, rerum tempore voluptates! E
+                        aque ipsa officia optio recusandae. Adipisci aliquid corporis
+                        distinctio dolore ea facilis ipsa laboriosam nostrum nulla officia
+                        placeat totam, voluptas. Aut debitis ipsum iure magnam ratione
+                        recusandae sapiente? Commodi ipsum itaque, nostrum odio quod ratione
+                        saepe vitae. Eveniet nostrum quasi unde veniam voluptatem!
+                        Accusamus animi asperiores aspernatur consequatur, culpa cupiditate
+                        et expedita inventore ipsum iure mollitia nisi numquam odit placeat
+                        quidem repudiandae rerum sapiente sint sit temporibus, veniam vitae
+                        voluptate? Culpa dicta dolorem esse, eveniet exercitationem fugiat
+                        itaque iure magni natus nemo nisi placeat, quaerat qui sit, suscipit
+                        unde vitae! Deleniti eligendi et, fuga magnam necessitatibus praesentium
+                        quas vero? Accusamus amet cumque facer
+                    </h3>
+
+                    <div className={styles.footer}>
+                        <p>contacts</p>
+                        <a className={styles.href2} href="/">WhatsApp</a>
+                        <a className={styles.href2} href="/">Instagram</a>
+                        <a className={styles.href2} href="/">Telegram</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
